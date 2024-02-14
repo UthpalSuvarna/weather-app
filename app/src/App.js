eff8f2f8e925604855e32b7afd4fe3330365c098
UthpalSuvarna <uthpal33@gmail.com>
2024-02-14 23:07:57 +0530

    Added Temp component

--- app/src/App.js
+++ app/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import TopButtons from "./components/TopButtons";
 import Search from "./components/Search";
 import TimeAndLocation from "./components/TimeAndLocation";
+import TempAndDetails from "./components/TempAndDetails";
 
 function App() {
   return (
     <div className="mx-auto max-w-screen py-5 px-10 bg-slate-950">
       <TopButtons />
-      <Search/>
-      <TimeAndLocation/>
+      <Search />
+      <TimeAndLocation />
+      <TempAndDetails />
     </div>
   );
 }
---
+++ app/src/components/TempAndDetails.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TempAndDetails() {
+  return (
+    <div>
+      <div className="flex items-center justify-center text-xl">
+        <p className="text-white font-extralight">Rainy/Sunny </p>
+      </div>
+    <div className="text-white flex flex-row justify-center items-center">19C</div>
+    </div>
+  );
+}
+
+export default TempAndDetails;
