d09ddd525c5aa02e96b48e3f21ce645970dcd8c4
UthpalSuvarna <uthpal33@gmail.com>
2024-02-14 22:52:28 +0530

    Added Time component

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import TopButtons from "./components/TopButtons";
 import Search from "./components/Search";
+import TimeAndLocation from "./components/TimeAndLocation";
 
 function App() {
   return (
     <div className="mx-auto max-w-screen py-5 px-10 bg-slate-950">
       <TopButtons />
       <Search/>
+      <TimeAndLocation/>
     </div>
   );
 }
---
+++ app/src/components/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TimeAndLocation() {
+  return (
+    <div>
+      <div className="flex justify-center items-center my-6">
+        <p className="text-white text-xl font-light">
+          Monday | 12 Jan | 12:00pm
+        </p>
+      </div>
+      <div className="flex justify-center items-center my-6">
+        <p className="text-white text-5xl font-bold my-5"> LONDON</p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeAndLocation;
